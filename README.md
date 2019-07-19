408b8cc0c557f2781a32cef0caf52e38897246b3
xeyal_i <xeyal.isaxanov@gmail.com>
2019-07-19 20:16:38 +0400

    Updates

---
+++ README.md
@@ -0,0 +1,11 @@
+Conway's Game of Life
+===========================
+
+This is a simple simulation of [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life).
+
+## How to Run
+
+Simply run `npm install` then `npm start`. A webpage will show in the browser.
+Click the board to setup the initial state, or use Random button to randomize the board.
+Then click "Run" button to see the iterations.
+
---
+++ precache-manifest.cccffca9bd568328b4c7ef69a1e359e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b833ef3ef8902277f0873085a09fd1d",
+    "url": "/reactgameoflife/index.html"
+  },
+  {
+    "revision": "8799be87f2074b0e6220",
+    "url": "/reactgameoflife/static/css/main.2262cc76.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/reactgameoflife/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8799be87f2074b0e6220",
+    "url": "/reactgameoflife/static/js/main.5930945a.chunk.js"
+  },
+  {
+    "revision": "f26b29a4a00c946bdb9c",
+    "url": "/reactgameoflife/static/js/runtime~main.9ff40349.js"
+  }
+]);
